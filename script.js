3290726ef954880db0c7f655f6e06f3f40923a4b
Alexander Tomelovich <tomelovichs@gmail.com>
2023-09-27 21:03:18 +0300

    ВТОРАЯ ПРАКТИКА (МОЙ ВАРИАНТ)

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-let films = {
 
-};
 let arr = [];
-
-let a = prompt("Один из последних просмотренных фильмов?", "");
-let b = +prompt("На сколько оцените его?","");
-let c = prompt("Один из последних просмотренных фильмов?", "");
-let d = +prompt("На сколько оцените его?","");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -17,6 +10,39 @@ const personalMovieDB = {
     privat: false
 };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 1; i<3; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", "");
+    let b = +prompt("На сколько оцените его?","");
+    if ((a == "" || a.length > 50) || (b == "" || b.length > 50)) {
+        i--;
+    }
+    else {
+        personalMovieDB.movies[a] = b;
+    }
+}
+
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log("Мало");
+}
+else if ((personalMovieDB.count > 9) && (personalMovieDB.count < 31)) {
+    console.log("Классика");
+}
+else {
+    console.log("Киноман");
+}
+// let i=1;
+
+// while (i<3) {
+//     let a = prompt("Один из последних просмотренных фильмов?", "");
+//     let b = +prompt("На сколько оцените его?","");
+//     if ((a == "" || a.length > 50) || (b == "" || b.length > 50)) {
+//         i--;
+//     }
+//     else {
+//         i++;
+//         personalMovieDB.movies[a] = b;
+//     }
+// }
+// console.log(personalMovieDB);
